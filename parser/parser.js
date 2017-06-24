3246f583e930b658ceec487fe13382530cbebbed
changhc <changhc84@gmail.com>
2017-06-24 19:45:06 +0800

    parsed files

--- parser/parser.js
+++ parser/parser.js
@@ -1,38 +1,60 @@
+const readline = require('readline');
+const pos = require('pos');
 const fs = require('fs');
-const request = require('request');
 
-if (process.argv.length < 4) {
-  throw new Error('Too few arguments!');
+if (process.argv.length !== 3) {
+  throw new Error('Should have only one argument!');
 }
 
-const API_KEY = process.argv[2];
-const startId = parseInt(process.argv[3], 10);
-const count = 400;
-const baseUrl = `http://food2fork.com/api/get?key=${API_KEY}&rId=`;
+const inputStream = fs.createReadStream(process.argv[2]);
 
-let promiseList = [];
-for (let i = 0; i < count; i += 1) {
-  const req = new Promise((resolve, reject) => {
-    const req = request({
-        url: baseUrl + (startId + i).toString(),
-        json: true,
-        timeout: 120000,
-      }, (err, res, body) => {
-        if (err || res.statusCode < 200 || res.statusCode > 299) {
-          reject(err);
-        }
-        resolve(body);
-      }
-    );
+const filter = ['/', 'cup', 'ounce', 'package', '&nbsp', 'tablespoon', 'teaspoon', 'degree', 'container', 'can', 'pound'];
+const parsed = [];
+let eof = false;
+
+const rl = readline.createInterface({
+  input: inputStream,
+});
+
+rl.on('close', () => {
+  fs.writeFile(process.argv[2] + '.out', parsed.join('\n'), (err) => {
+    if (err) throw err;
+    console.log('Done.');
   });
-  promiseList.push(req);
+});
 
-}
+rl.on('line', (input) => {
+  const ingredients = JSON.parse(input).recipe.ingredients;
+  if (ingredients === undefined) return;
+  const ingredient = [];
+  for (let i = 0; i < ingredients.length; i+= 1) {
+    const cleanedWords = [];
+    ingredients[i].replace(/\(.*\)/g, '');
+    const index = ingredients[i].indexOf(',');
+    if (index !== -1) {
+      ingredients[i] = ingredients[i].substr(0, index);
+    }
 
-Promise.all(promiseList).then(values => {
-  const writer = fs.createWriteStream(`${startId}-${startId + count - 1}.txt`);
-  writer.on('error', (err) => console.error(err));
-  values.forEach((value) => { writer.write(JSON.stringify(value) + '\n'); });
-  writer.end();
-})
-.catch(err => console.error(err));
+    var words = new pos.Lexer().lex(ingredients[i]);
+    var tagger = new pos.Tagger();
+    var taggedWords = tagger.tag(words);
+    for (let j in taggedWords) {
+      var taggedWord = taggedWords[j];
+      var word = taggedWord[0];
+      var tag = taggedWord[1];
+      if (tag === 'NNS') {
+        word = word.substr(0, word.length - 1);
+        tag = 'NN';
+      }
+      word = word.replace(/[&\*]/g, '');
+      if (tag !== 'NN' || filter.indexOf(word) !== -1 || word.length === 1) {
+        continue;
+      }
+      cleanedWords.push(word.toLowerCase());
+    }
+    const concatWords = cleanedWords.join(' ');
+    if (cleanedWords.length === 0 || ingredient.indexOf(concatWords) !== -1) continue;
+    ingredient.push(concatWords);
+  }
+  parsed.push(ingredient.join(','));
+});
