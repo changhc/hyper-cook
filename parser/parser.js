2058b3f2818d7eee8c6971f87399220080db82ff
changhc <changhc84@gmail.com>
2017-06-25 11:23:28 +0800

    sql script

--- parser/parser.js
+++ parser/parser.js
@@ -8,26 +8,51 @@ if (process.argv.length !== 3) {
 
 const inputStream = fs.createReadStream(process.argv[2]);
 
-const filter = ['/', 'cup', 'ounce', 'package', '&nbsp', 'tablespoon', 'teaspoon', 'degree', 'container', 'can', 'pound'];
+const filter = ['/', 'cup', 'ounce', 'package', '&nbsp', 'tablespoon', 'teaspoon', 'degree', 'container', 'can', 'pound', 'inch'];
 const parsed = [];
 const dictionay = {};
 let eof = false;
+
+const createScript = () => {
+  let script = 'CREATE TABLE recipe (\n\ttitle\tVARCHAR(100) NOT NULL,\n\turl\tVARCHAR(200) NOT NULL,\n\timg\tTEXT,\n';
+  for (key in dictionay) {
+    if (key === '') console.log(1);
+    script += `\t${key}\tBOOLEAN,\n`;
+  }
+  script = script.substr(0, script.length - 2);
+  script += '\n);\n';
+  for (index in parsed) {
+    const recipe = parsed[index];
+    const title = recipe.title.replace(/'/g, '\'\'');
+    script += `INSERT INTO recipe (title, url, img,${recipe.ingredient.join(',')}) VALUES ('${title}', '${recipe.url}', '${recipe.img}', ${Array(recipe.ingredient.length).fill('TRUE').join(',')});\n`;
+  }
+  return script;
+}
 
 const rl = readline.createInterface({
   input: inputStream,
 });
 
 rl.on('close', () => {
-  fs.writeFile(process.argv[2] + '.out', parsed.join('\n'), (err) => {
+  fs.writeFile(process.argv[2] + '.out', JSON.stringify(parsed), (err) => {
     if (err) throw err;
     console.log('Done.');
   });
-  console.log(Object.keys(dictionay).length)
+  fs.writeFile('data/script', createScript(), (err) => {
+    if (err) throw err;
+    console.log('script created.');
+  });
 });
 
 rl.on('line', (input) => {
+  const raw = JSON.parse(input).recipe;
+  if (raw === undefined) return;
   const ingredients = JSON.parse(input).recipe.ingredients;
   if (ingredients === undefined) return;
+  const recipe = {};
+  recipe.title = raw.title;
+  recipe.url = raw.f2f_url;
+  recipe.img = raw.image_url;
   const ingredient = [];
   for (let i = 0; i < ingredients.length; i+= 1) {
     const cleanedWords = [];
@@ -52,16 +77,18 @@ rl.on('line', (input) => {
         }
         tag = 'NN';
       }
-      word = word.replace(/[&\*]/g, '');
+      word = word.replace(/[&\*%]/g, '');
+      word = word.replace(/-/g, '_');
       if (tag !== 'NN' || filter.indexOf(word) !== -1 || word.length === 1) {
         continue;
       }
       cleanedWords.push(word.toLowerCase());
     }
-    const concatWords = cleanedWords.join(' ');
-    if (cleanedWords.length === 0 || ingredient.indexOf(concatWords) !== -1) continue;
+    const concatWords = cleanedWords.join('_');
+    if (concatWords.length === 0 || ingredient.indexOf(concatWords) !== -1) continue;
     ingredient.push(concatWords);
     dictionay[concatWords] = 1;
   }
-  parsed.push(ingredient.join(','));
+  recipe.ingredient = ingredient;
+  parsed.push(recipe);
 });
