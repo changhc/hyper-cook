a7835a57504a2de35f3ee4cc13cb5bd1a846c096
Katherine Tsai <b02901149@ntu.edu.tw>
2017-06-29 17:37:08 +0800

    modify server home

--- page/routes/home.js
+++ page/routes/home.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:user', function (req, res) {
-  
-  if(req.session.loggedIn){
+  const name = req.params.user;
+  if(req.session.loggedIn === name ){
     console.log('direct to main page');
     res.render('index', { layout: 'index.html' });
   } else {
